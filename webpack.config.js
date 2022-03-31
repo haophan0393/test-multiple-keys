624e61fcc8312542b278dd594f1003141c28e5df
Hao Phan <hao@addsearch.com>
2022-03-31 14:14:18 +0300

    init package.json

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const TerserJSPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
+const webpack = require('webpack');
+const PACKAGE = require('./package.json');
+const banner = PACKAGE.name + ' ' + PACKAGE.version;
+
+module.exports = {
+  entry: ['./src/ui.scss', './src/ui.js'],
+  output: {
+    filename: 'ui.min.js'
+  },
+  mode: 'production',
+  devtool: 'source-map',
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCssAssetsPlugin({})],
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: banner
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'ui.min.css'
+    }),
+    new FileManagerPlugin({
+      onEnd: {
+        archive: [
+          {
+            source: '{*.*,dist/*,src/*}',
+            destination: 'installation.zip'
+          }
+        ]
+      }
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {'targets': '> 0.1%, IE 10, not dead'}
+              ]
+            ]
+          }
+        }
+      },
+
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: "css-loader", options: {} },
+          {
+            loader: "postcss-loader",
+            options: {
+              ident: 'postcss',
+              plugins: [
+                require('autoprefixer')({}),
+              ]
+            }
+          },
+          { loader: "sass-loader", options: {} }
+        ],
+      }
+    ]
+  }
+};
